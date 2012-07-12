86d6e5c8e654a9ebdd1cee2b6b7112102ee4b14a
Andreas Marschke <andreas.marschke@gmail.com>
2012-07-12 11:28:04 +0200

    perl/csv/csv_builder.pl: now understands utf8 and prints select statement results in a mysql-ish ascii-table

--- perl/csv/csv_builder.pl
+++ perl/csv/csv_builder.pl
@@ -10,7 +10,7 @@ csv_builder -p ./csv -q "create table mytable(id INTEGER, name CHAR (10))"
 
 csv_builder -p ./csv -q "insert into mytable (id, name) values(1, 'frank')"
 
-csv_builder -p ./csv -q "select * from mytable;"
+csv_builder -p ./csv -q "select * from mytable;" -d single
 
 =head1 DESCRIPTION
 
@@ -23,17 +23,18 @@ use 5.010_000;
 
 use strict;
 use warnings;
+use utf8;
 
 use Getopt::Long;
+use Data::Dumper;
 use Pod::Usage;
 use DBI;
 
 our $VERSION = '0.1';
-
 my ($path,   # path to the csv data
     $query,  # query to be sent
     $dbh,    # db handler
-    $sth     # statement handler
+    $sth    # statement handler
    );
 
 my $result = GetOptions (
@@ -42,8 +43,6 @@ my $result = GetOptions (
   "h|help"           => sub { pod2usage(-exitval   => 0,
 					-verbose   => 99,
 					-noperldoc => 1) });
-
-
 die "ERROR: No such directory $path" if ( ! -d $path );
 
 $dbh = DBI->connect ("dbi:CSV:",undef, undef,
@@ -56,5 +55,22 @@ $dbh = DBI->connect ("dbi:CSV:",undef, undef,
 		      PrintError   => 1,
 		     }) or die "$DBI::errstr";
 
-$dbh->do ($query) or die $dbh->errstr();
+$sth = $dbh->prepare($query);
+$sth->execute();
+# if its a select statement
+if ($sth->{NUM_OF_FIELDS} gt 0 ) {
+  use Text::TabularDisplay;
+  my $t = Text::TabularDisplay->new();
+  my $row = $sth->fetchrow_hashref;
+  $t->columns(keys $row);
+  $t->add(values $row);
+  while (my @row = $sth->fetchrow) {
+    $t->add(@row);
+  }
+  my $rendered_text = $t->render();
+  utf8::encode($rendered_text);
+  print  $rendered_text;
+  print "\n";
+}
+$sth->finish ();
 $dbh->disconnect();
